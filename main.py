92572ea1838c8c6f909362dc11819cdf6a8f4ed6
AleksanderSv <zzaws@mail.ru>
2023-02-06 10:15:01 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = input("Введите сумму вклада:")
+bankТКБ = int(money) * (per_cent['ТКБ']) / 100
+banкСКБ =int(money) * (per_cent['СКБ']) / 100
+bankВТБ =int(money) * (per_cent['ВТБ']) / 100
+bankСБЕР =int(money) * (per_cent['СБЕР']) / 100
+print('ТКБ-', (bankТКБ))
+print('СКБ-', (banкСКБ))
+print('ВТБ-', (bankВТБ))
+print('СБЕР-', (bankСБЕР))
+deposit = [int(bankТКБ), int(banкСКБ), int(bankВТБ), int(bankСБЕР)]
+depositi = max(deposit)
+print("Максимальный сумма:", (depositi))
